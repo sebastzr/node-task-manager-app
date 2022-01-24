920f0dd54435885b366c22147689d2e085d313d3
Sebastian Zapata <sebastzr@utp.edu.co>
2022-01-24 15:50:28 -0500

    feat: Add delete task functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -35,8 +35,17 @@ const updateTask = (req, res) => {
   res.json({ id: req.params.id });
 };
 
-const deleteTask = (req, res) => {
-  res.json({ id: req.params.id });
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
