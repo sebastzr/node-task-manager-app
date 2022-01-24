3c821cd0c2ff73542d46d7ac847a5dd15476801e
Sebastian Zapata <sebastzr@utp.edu.co>
2022-01-24 02:19:06 -0500

    First commit: setup router, controller and basic app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
