835967201c7a95b87056c70bcc3c56914583e1cc
Sebastian Zapata <sebastzr@utp.edu.co>
2022-01-24 02:57:33 -0500

    feat: setup mongo db connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middleware
 
@@ -11,6 +13,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
